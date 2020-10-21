6d83ea1178e79a327d2230dfbee5188650fca3ac
DaniloSVJ <danilocotatoceu@gmail.com>
2020-10-20 23:01:45 -0300

    Parkles

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTD {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,44 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+     return this.transactions
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (acumulator,transaction)=>{
+        switch(transaction.type){
+          case "income":
+            acumulator.income+=transaction.value
+            break
+          case "outcome":
+            acumulator.outcome+=transaction.value
+            break
+          default:
+            break
+
+        }
+        acumulator.total =  acumulator.income -  acumulator.outcome
+        return acumulator
+    },{
+      income:0,
+      outcome:0,
+      total:0
+    })
+    //this.transactions.push(balance)
+    return balance
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title,value,type}:CreateTransactionDTD): Transaction {
+
+    const transaction = new Transaction({
+      title,
+      value,
+      type
+  })
+    this.transactions.push(transaction)
+
+    return transaction
   }
 }
 
